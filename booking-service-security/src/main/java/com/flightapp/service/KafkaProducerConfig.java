9c03ade8e00850722adc3426c61c29c92e0643e0
Poojithamodala <poojithamodala26@gmail.com>
2025-12-08 16:56:42 +0530

    Updated

--- booking-service-security/src/main/java/com/flightapp/service/KafkaProducerConfig.java
+++ booking-service-security/src/main/java/com/flightapp/service/KafkaProducerConfig.java
@@ -1,15 +1,18 @@
 package com.flightapp.service;
 
-import com.flightapp.messaging.BookingEvent;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.flightapp.messaging.BookingEvent;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -17,7 +20,7 @@ public class KafkaProducerConfig {
 	@Bean
 	public ProducerFactory<String, BookingEvent> bookingEventProducerFactory() {
 		Map<String, Object> config = new HashMap<>();
-		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
 		config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 		return new DefaultKafkaProducerFactory<>(config);
