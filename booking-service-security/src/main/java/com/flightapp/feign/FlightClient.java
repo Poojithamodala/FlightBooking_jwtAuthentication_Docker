21cbce814b7d0152b758c716aa4df702694a911e
Poojithamodala <poojithamodala26@gmail.com>
2025-12-08 16:54:34 +0530

    Updated

--- booking-service-security/src/main/java/com/flightapp/feign/FlightClient.java
+++ booking-service-security/src/main/java/com/flightapp/feign/FlightClient.java
@@ -4,18 +4,21 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.flightapp.dto.FlightDto;
 
-@FeignClient(name = "flight-service")
+@FeignClient(name = "flight-service", configuration = FeignClientConfig.class)
 public interface FlightClient {
 
 	@GetMapping("/api/flight/{id}")
-	FlightDto getFlight(@PathVariable String id);
+	FlightDto getFlight(@PathVariable String id, @RequestHeader("Authorization") String auth);
 
 	@PutMapping("/api/flight/internal/{id}/reserve/{seatCount}")
-	Object reserveSeats(@PathVariable String id, @PathVariable int seatCount);
+	Object reserveSeats(@PathVariable String id, @PathVariable int seatCount,
+			@RequestHeader("Authorization") String auth);
 
 	@PutMapping("/api/flight/internal/{id}/release/{seatCount}")
-	Object releaseSeats(@PathVariable String id, @PathVariable int seatCount);
+	Object releaseSeats(@PathVariable String id, @PathVariable int seatCount,
+			@RequestHeader("Authorization") String auth);
 }
