cae6245d4bdecb81e98fe54dfb7e3b4bc8e350bb
Poojithamodala <poojithamodala26@gmail.com>
2025-12-08 16:56:00 +0530

    Updated

--- booking-service-security/src/main/java/com/flightapp/service/BookingService.java
+++ booking-service-security/src/main/java/com/flightapp/service/BookingService.java
@@ -1,21 +1,22 @@
 package com.flightapp.service;
 
+import java.util.List;
+
 import com.flightapp.model.FLIGHTTYPE;
 import com.flightapp.model.Passenger;
 import com.flightapp.model.Ticket;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.List;
 
 public interface BookingService {
 
 	Mono<String> bookTicket(String userEmail, String departureFlightId, String returnFlightId,
-			List<Passenger> passengers, FLIGHTTYPE tripType);
+			List<Passenger> passengers, FLIGHTTYPE tripType, String token);
 
 	Mono<Ticket> getByPnr(String pnr);
 
 	Flux<Ticket> historyByEmail(String email);
 
-	Mono<String> cancelByPnr(String pnr);
+	Mono<String> cancelByPnr(String pnr, String token);
 }
