c18fce8db5cf8c8ec9323aed5ba72098ee5cc575
Poojithamodala <poojithamodala26@gmail.com>
2025-12-08 16:59:37 +0530

    Updated

--- config-server-webflux-security/src/main/java/com/flightapp/ConfigServerWebfluxApplication.java
+++ config-server-webflux-security/src/main/java/com/flightapp/ConfigServerWebfluxApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableConfigServer 
 public class ConfigServerWebfluxApplication {
 
 	public static void main(String[] args) {
