264f788f61375ac319092aaf62dc1e0bac00a260
Poojithamodala <poojithamodala26@gmail.com>
2025-12-23 17:15:54 +0530

    Updated booking service

--- booking-service-security/src/test/java/com/flightapp/BookingServiceApplicationTests.java
+++ booking-service-security/src/test/java/com/flightapp/BookingServiceApplicationTests.java
@@ -1,9 +1,10 @@
 package com.flightapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class BookingServiceApplicationTests {
 
 //	@Test
